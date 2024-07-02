3cfdcd43beb4f23bbf76f1e21e911b0f23ae66cd
Herschelle42 <github@threemillion.net>
2024-07-03 09:07:21 +1000

    add retrieval of resource content, export content to file

--- api-samples/scratch run wf and get logs.ps1
+++ api-samples/scratch run wf and get logs.ps1
@@ -7,6 +7,7 @@
   Assumes the following variables have been created:
   $vraServer
   $headers
+  $outputDirectory
 
   This example is specifically around the AA8 onBoarding package of workflows 
   and process but the same logic can be applied to other workflows
@@ -39,7 +40,9 @@
 #>
 Return
 
+
 #Get the id of the Workflow by looking it up by name
+
 $wfName = "2. captureDiscoveredMachineDatavRA8"
 
 $uri = "https://$($vraServer)/vco/api/workflows?maxResult=10&startIndex=0&conditions=name^$($wfName)"
@@ -68,7 +71,53 @@ $response = Invoke-RestMethod -Method $method -Uri $uri -Headers $headers
 $logText = $response.logs.entry | ? { $_.'short-description' -match "machine_data" } | Select -ExpandProperty "short-description"
 #Resource Element //tmp/machine_data_27-Jun-2024-T235733_discovered.csv Saved to Folder Onboarding
 
-$filename = $logText.Split("/")[3].split(" ")[0]
+$logname = $logText.Split("/")[3].split(" ")[0]
 #machine_data_27-Jun-2024-T235733_discovered.csv
 
-#next step would be to get the configuration item by the file name
+
+#Get the resource item by name
+$selResourceName = @{Name="ResourceName"; Expression={$_.attributes | ? { $_.name -eq "name" } | Select -ExpandProperty Value}}
+$selResourceId = @{Name="ResourceId"; Expression={$_.attributes | ? { $_.name -eq "id" } | Select -ExpandProperty Value}}
+
+
+$method="GET"
+$uri = "https://$($vraServer)/vco/api/resources"
+#$escapedUri = [uri]::EscapeUriString($uri)
+try
+{
+    $response = $null
+    $response = Invoke-RestMethod -Method $method -Uri $uri -Headers $headers
+} catch {
+    Write-Output "Error Exception Code: $($_.exception.gettype().fullname)"
+    Write-Output "Error Message:        $($_.ErrorDetails.Message)"
+    Write-Output "Exception:            $($_.Exception)"
+    Write-Output "StatusCode:           $($_.Exception.Response.StatusCode.value__)"
+    throw
+}
+
+#Get the resourceId of the log file
+$resourceId = $response.link | Select $selResourceName, $selResourceId | ? { $_.ResourceName -eq $logName } | Select -ExpandProperty ResourceId
+
+
+$method="GET"
+$uri = "https://$($vraServer)/vco/api/resources/$($resourceId)"
+#$escapedUri = [uri]::EscapeUriString($uri)
+try
+{
+    $response = $null
+    $response = Invoke-RestMethod -Method $method -Uri $uri -Headers $headers
+} catch {
+    Write-Output "Error Exception Code: $($_.exception.gettype().fullname)"
+    Write-Output "Error Message:        $($_.ErrorDetails.Message)"
+    Write-Output "Exception:            $($_.Exception)"
+    Write-Output "StatusCode:           $($_.Exception.Response.StatusCode.value__)"
+    throw
+}
+
+
+#split into individual items if want to do search by line.
+#$resourceContent = $response.Split("`n")
+
+
+#export \ save to a local file
+$response | Out-File -FilePath "$($outputDirectory)\$($logname)"  -Encoding ascii
