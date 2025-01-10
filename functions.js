0422c5f996baf76c0ec52c8e9ed0029eea111900
Herschelle42 <github@threemillion.net>
2025-01-10 14:46:44 +1100

    add convertVcVMToProperties

--- functions.js
+++ functions.js
@@ -20,3 +20,45 @@ function getISODateTime(){
     ('0' + 	date.getSeconds()	).slice(-2) 
     return dateTimeString;
 }
+
+function convertVcVmToProperties(vm){
+    var vmProps = {
+        vimType: vm.vimType,
+        overallCpuUsage: vm.overallCpuUsage,
+        memory: vm.memory,
+        vimId: vm.vimId,
+        instanceId: vm.instanceId,
+        hostMemoryUsage: vm.hostMemoryUsage,
+        guestMemoryUsage: vm.guestMemoryUsage,
+        vmToolsVersionStatus: vm.vmToolsVersionStatus,
+        biosId: vm.biosId,
+        productVendor: vm.productVendor,
+        isTemplate: vm.isTemplate,
+        vmToolsStatus: vm.vmToolsStatus,
+        name: vm.name,
+        displayName: vm.displayName,
+        unsharedStorage: vm.unsharedStorage,
+        id: vm.id,
+        cpu: vm.cpu,
+        connectionState: vm.connectionState,
+        committedStorage: vm.committedStorage,
+        hostName: vm.hostName,
+        type: vm.type,
+        sdkId: vm.sdkId,
+        guestHeartbeatStatus: vm.guestHeartbeatStatus,
+        overallStatus: vm.overallStatus,
+        totalStorage: vm.totalStorage,
+        guestOS: vm.guestOS,
+        productFullVersion: vm.productFullVersion,
+        configStatus: vm.configStatus,
+        ipAddress: vm.ipAddress,
+        annotation: vm.annotation,
+        productName: vm.productName,
+        state: vm.state,
+        mem: vm.mem,
+        memoryOverhead: vm.memoryOverhead,
+        alarmActionsEnabled: vm.alarmActionsEnabled,
+        vmVersion: vm.vmVersion
+    }
+    return vmProps
+}
